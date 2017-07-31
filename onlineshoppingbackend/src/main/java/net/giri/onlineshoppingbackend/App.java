87b915e0c4a94c5217ea721cba01773062929594
Giridhar <giridharku@gmail.com>
2017-07-31 20:11:50 +1200

    First commit

---
+++ onlineshoppingbackend/src/main/java/net/giri/onlineshoppingbackend/App.java
@@ -0,0 +1,13 @@
+package net.giri.onlineshoppingbackend;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
